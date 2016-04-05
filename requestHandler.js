b0fb74c1c0a732002d42a48ae24e9db9b1da3561
Joel Aguero <joelmaguero@gmail.com>
2016-04-04 21:32:53 -0700

    Console log response text from req

---
+++ requestHandler.js
@@ -0,0 +1,19 @@
+var _makeGetRequest = function(url, callback) {
+  $.ajax({
+    url: url,
+    type: 'GET',
+    success: function(res) {
+      callback(null, res.responseText);
+    },
+    error: function(err) {
+      callback(err);
+    }
+  });
+};
+
+var getUrlMetaData = function(url, callback, tag) {
+    _makeGetRequest(url, function(err, data) {
+      if (err) throw err;
+      callback(null, data);
+    })
+};
