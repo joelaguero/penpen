55fad34e483119d010041f379278fb0b0645ba53
Joel Aguero <joelmaguero@gmail.com>
2016-04-05 12:40:49 -0700

    Render article cards and open link in new window

--- ArticleListEntry.jsx
+++ ArticleListEntry.jsx
@@ -3,15 +3,27 @@ import React from 'react';
 class ArticleListEntry extends React.Component{
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    window.open(
+      this.props.article.url,
+      '_blank'
+    );
   }
 
   render() {
+    var article = this.props.article;
     return (
-      <div style={{border: '1px solid', padding: '10px'}}>
-        <img src="//placekitten.com/100/100"/>
-        <h2>Article Title</h2>
-        <p>{'http://articleurl.com/url/12345'}</p>
-        <p>{'This is a short description of the article. It describes something about it. It is hopefully useful and not too long, or we might have to automatically truncate.'}</p>
+      <div onClick={this.handleClick} style={{margin: '10px 0px', 'max-width': '40%', border: '1px solid'}}>
+        <img style={{'max-width': '100%'}}src={article.image}/>
+        <div style={{'margin': '10px'}}>
+          <h2 style={{'margin': '5px 0px'}}>{article.title}</h2>
+          <p style={{'white-space': 'nowrap', 'overflow': 'hidden', 'text-overflow': 'ellipsis', 'text-transform':'uppercase', 'color': 'gray', 'font-family':'sans-serif','font-size': '11px', 'margin': '0', 'padding': '0'}}>{article.site_name} · {article.url}</p>
+          <p style={{'margin-bottom': '0'}}>{article.description}</p>
+        </div>
       </div>
     );
   }
