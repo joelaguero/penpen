f35dd6f63c1627731f080717442949c16fc71ad2
Joel Aguero <joelmaguero@gmail.com>
2016-04-05 15:30:19 -0700

    Implement database entry with post for articles

--- articles/articleController.js
+++ articles/articleController.js
@@ -15,8 +15,17 @@ module.exports = {
   newArticle: function(req, res) {
     console.log('Inside new article!');
     console.log(req.body);
+    var data = req.body;
+
+    var article = new Article();
+    article.title = data.title;
+    article.description = data.description;
+    article.url = data.url;
+    article.image = data.image;
+    article.save();
+
+    console.log("This article should have just been added to the database: ", article);
     res.sendStatus(200);
-    // var article = new Article();
   }
 
 };
