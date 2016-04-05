787cc529697f9274fa6670a76b99be2bb21df18f
Joel Aguero <joelmaguero@gmail.com>
2016-04-05 11:31:43 -0700

    Call addArticle fn in ArticleCreator

--- App.jsx
+++ App.jsx
@@ -6,14 +6,24 @@ import ArticleCreator from './ArticleCreator.jsx';
 class App extends React.Component{
   constructor(props) {
     super(props);
+    this.state = {
+      articles: []
+    };
+  }
+
+  addArticle(article) {
+    console.log(article);
+    // this.setState({
+    //   articles: this.props.articles.push(article)
+    // });
   }
 
   render() {
     return(
       <div>
         <h1>Welcome to Penpen, {this.props.name}!</h1>
-        <ArticleCreator/>
-        <ArticleList/>
+        <ArticleCreator addArticle={this.addArticle.bind(this)} />
+        <ArticleList articles={this.state.articles} />
       </div>
     );
   }
--- ArticleCreator.jsx
+++ ArticleCreator.jsx
@@ -4,20 +4,14 @@ import React from 'react';
 class ArticleCreator extends React.Component {
   constructor(props) {
     super(props);
-    this.handleAdd = this.handleAdd.bind(this);
     this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleAdd(article) {
-    console.log(article);
-    // this.props.addArticle.bind(this));
   }
 
   handleClick(e) {
     e.preventDefault();
     getUrlMetaData(this.refs.targetUrl.value, function(err, article) {
-      console.log(article);
-    });
+      this.props.addArticle(article);
+    }.bind(this) );
   }
 
   render() {
