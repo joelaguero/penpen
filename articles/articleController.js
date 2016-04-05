2b02f87d451eba4352d9cde256f60fb8c084731a
Joel Aguero <joelmaguero@gmail.com>
2016-04-05 15:02:30 -0700

    Implement server support for all articles

--- articles/articleController.js
+++ articles/articleController.js
@@ -2,20 +2,20 @@ var Article = require('./articleModel.js');
 
 module.exports = {
   allArticles: function(req, res) {
-    res.status(200);
-    res.send('Hello World');
-    // findAllArticles({})
-    //   .then(function(articles) {
-    //     res.send('fish')
-    //   })
-    //   .catch(function(error) {
-    //     console.log(error);
-    //   })
+    Article.find({})
+      .then(function(articles) {
+        res.status(200);
+        res.send(articles)
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
   },
 
   newArticle: function(req, res) {
-    console.log('REQUEST BODY');
+    console.log('Inside new article!');
     console.log(req.body);
+    // var article = new Article();
   }
 
 };
