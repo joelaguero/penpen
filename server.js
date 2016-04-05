4777770e8890fc70cf4d1ec0a54c494e84234ef9
Joel Aguero <joelmaguero@gmail.com>
2016-04-04 18:57:22 -0700

    Render index.html with React

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
 var port = 3000;
 
+app.use(express.static(__dirname));
+
 app.get('/', function(req, res) {
-  res.send('Welcome to Penpen!');
+  res.sendFile('index.html');
+  // res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, function() {
