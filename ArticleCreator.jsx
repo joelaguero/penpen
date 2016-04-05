c6545af0802653610b7bc3ab1f2150ca087de00c
Joel Aguero <joelmaguero@gmail.com>
2016-04-05 11:21:51 -0700

    Refactor to bind in constructor

--- ArticleCreator.jsx
+++ ArticleCreator.jsx
@@ -4,29 +4,36 @@ import React from 'react';
 class ArticleCreator extends React.Component {
   constructor(props) {
     super(props);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+  handleAdd(article) {
+    console.log(article);
+    // this.props.addArticle.bind(this));
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+
+    getUrlMetaData(this.refs.targetUrl.value, function(err, data) {
+      var el = $( '<div></div>' ).html(data);
+      var article = new Object;
+
+      el.find("meta[property*='og']")
+      .each(function(key, val) {
+        var property = this.getAttribute('property').substr(3);
+        article[property] = val.content;
+        console.log(article);
+      });
+      // handleAdd(article);
+    });
   }
 
   render() {
-    var handleClick = function(e) {
-      e.preventDefault();
-
-      getUrlMetaData(this.refs.targetUrl.value, function(err, data) {
-        var el = $( '<div></div>' ).html(data);
-        var article = new Object;
-
-        el.find("meta[property*='og']")
-          .each(function(key, val) {
-            var property = this.getAttribute('property').substr(3);
-            article[property] = val.content;
-
-            window.storage.push(article);
-          });
-      });
-    }
 
     return(
-      <form onSubmit={handleClick.bind(this)}>
+      <form onSubmit={this.handleClick}>
         <input type='url' ref='targetUrl' placeholder='Enter a URL'/>
         <button type='submit' >Create Article</button>
       </form>
