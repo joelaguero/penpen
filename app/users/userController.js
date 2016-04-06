8c2787eb83e76f7652b5da3f20999517af51379d
Joel Aguero <joelmaguero@gmail.com>
2016-04-06 00:15:34 -0700

    Implement api/users endpoint

--- app/users/userController.js
+++ app/users/userController.js
@@ -1,5 +1,14 @@
 var User = require('./userModel.js');
 
 module.exports = {
-
+  allUsers: function(req, res) {
+    User.find({})
+      .then(function(data) {
+        res.status(200);
+        res.send(data);
+      })
+      .catch(function(err) {
+        console.log(err);
+      })
+  }
 };
--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,11 @@
 var articlesController = require('../app/articles/articleController.js');
-var usersController = require('../app/users/userController.js');
+var userController = require('../app/users/userController.js');
 
 module.exports = function(app, express) {
   app.get('/api/', function(req, res) {
     res.json({ message: 'This is the API!'});
   });
   app.get('/api/articles', articlesController.allArticles);
+  app.get('/api/users', userController.allUsers);
   app.post('/api/articles', articlesController.newArticle);
 };
