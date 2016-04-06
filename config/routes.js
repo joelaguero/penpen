f61b633d70505562980c5ac99eb4ddec966cd1a1
Joel Aguero <joelmaguero@gmail.com>
2016-04-06 09:16:05 -0700

    Refactor api routes to use router middleware

--- config/routes.js
+++ config/routes.js
@@ -3,16 +3,20 @@ var userController = require('../app/users/userController.js');
 var authenticate = require('./auth.js');
 
 module.exports = function(app, express) {
-  app.get('/api/', function(req, res) {
+  var apiRoutes = express.Router();
+
+  apiRoutes.get('/', function(req, res) {
     res.json({ message: 'This is the API!'});
   });
 
-  app.get('/api/articles', articlesController.allArticles);
-  app.post('/api/articles', articlesController.newArticle);
+  apiRoutes.get('/articles', articlesController.allArticles);
+  apiRoutes.post('/articles', articlesController.newArticle);
 
-  app.get('/api/users', userController.allUsers);
+  apiRoutes.get('/users', userController.allUsers);
 
-  app.post('/api/authenticate', authenticate);
+  apiRoutes.post('/authenticate', authenticate);
+
+  app.use('/api', apiRoutes);
 
   // ========
   // Testing
